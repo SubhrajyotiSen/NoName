860a3aa128da273c19da9d66dd4e4458e2c767bb
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2018-09-24 00:00:22 +0530

    chore: introduce lambda expressions

--- app/src/main/java/me/subhrajyoti/myday/MainActivity.java
+++ app/src/main/java/me/subhrajyoti/myday/MainActivity.java
@@ -1,25 +1,18 @@
 package me.subhrajyoti.myday;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.widget.Toast;
-
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 import me.subhrajyoti.myday.adapters.BirthdayAdapter;
-import me.subhrajyoti.myday.adapters.ClickListener;
 import me.subhrajyoti.myday.adapters.ProjectsAdapter;
-import me.subhrajyoti.myday.data.pojo.BirthdayModel;
-import me.subhrajyoti.myday.data.pojo.ProjectModel;
 import me.subhrajyoti.myday.data.remote.ApiService;
 
 public class MainActivity extends AppCompatActivity {
@@ -44,22 +37,12 @@ public class MainActivity extends AppCompatActivity {
 
         apiService = Utils.getApiService();
 
-        birthdayAdapter = new BirthdayAdapter( new BirthdayAdapter.ClickListener() {
-            @Override
-            public void onPositionClicked(int position) {
-                Toast.makeText(MainActivity.this, "Wishing Happy Birthday", Toast.LENGTH_SHORT).show();
-            }
-        });
+        birthdayAdapter = new BirthdayAdapter(position -> Toast.makeText(MainActivity.this, "Wishing Happy Birthday", Toast.LENGTH_SHORT).show());
 
         birthdaysRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
         birthdaysRecyclerView.setAdapter(birthdayAdapter);
 
-        projectsAdapter = new ProjectsAdapter( new ClickListener() {
-            @Override
-            public void onPositionClicked(int position) {
-                Toast.makeText(MainActivity.this, "Project tapped", Toast.LENGTH_SHORT).show();
-            }
-        });
+        projectsAdapter = new ProjectsAdapter(position -> Toast.makeText(MainActivity.this, "Project tapped", Toast.LENGTH_SHORT).show());
 
         projectRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
         projectRecyclerView.setAdapter(projectsAdapter);
@@ -69,48 +52,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @SuppressLint("CheckResult")
     private void fetchProjects() {
             apiService.loadProjects()
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(new SingleObserver<List<ProjectModel>>() {
-                        @Override
-                        public void onSubscribe(Disposable d) {
-
-                        }
-
-                        @Override
-                        public void onSuccess(List<ProjectModel> projects) {
-                            projectsAdapter.addAll(projects);
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-                            Log.d("Retrofit", e.getLocalizedMessage());
-                        }
-                    });
+                    .subscribe(projectModels -> projectsAdapter.addAll(projectModels));
     }
 
+    @SuppressLint("CheckResult")
     private void fetchBirthdays() {
         apiService.loadBirthdays()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new SingleObserver<List<BirthdayModel>>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onSuccess(List<BirthdayModel> birthdays) {
-                        birthdayAdapter.addAll(birthdays);
-                        Log.d("Retrofit", String.valueOf(birthdays.size()));
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.d("Retrofit", e.getLocalizedMessage());
-                    }
-                });
+                .subscribe(birthdayModels -> birthdayAdapter.addAll(birthdayModels));
     }
 }
